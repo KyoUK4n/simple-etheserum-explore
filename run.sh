424c72ba8fe6aa509a6539e185eba67ea22a34c4
kyouk4n <kyouk4n@gmail.com>
2026-06-22 16:50:24 +0800

    feat: add docker up in run.sh

--- run.sh
+++ run.sh
@@ -10,11 +10,15 @@
 #abigen --abi abi/ERC20.json --pkg=v1 --type ERC20 --out=pkg/contracts/v1/erc20.go
 #abigen --v2 --abi abi/ERC20.json --pkg=v2 --type ERC20 --out=pkg/contracts/v2/erc20.go
 
+# startup mysql service
+docker-compose -f docker/docker-compose.yaml up -d
+
+# build frontend asserts
 cd web
 npm install
 npm run build
 
+# startup backend api service
 cd ../
-
 go mod tidy && go mod vendor
 go run .
